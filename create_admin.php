42cdc3f640241709a3aca41db426ba1246eb565d
collins732 <collins.lemogo-kamnen@student.junia.com>
2026-02-01 16:34:51 +0100

    Remove legacy prototype and debug PHP files

--- create_admin.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Script pour créer le hash bcrypt du mot de passe admin
-$password = 'admin123';
-$hash = password_hash($password, PASSWORD_BCRYPT);
-echo "Hash bcrypt pour 'admin123':\n";
-echo $hash . "\n";
-?>
